c24f9d8884ce330ae5446ad7e7c1dd1819db25fc
Your Name <you@example.com>
2022-02-24 12:07:32 +0200

    node2 with error handeling and joi

--- index.js
+++ index.js
@@ -1,40 +1,10 @@
-import * as fs from "fs";
 import express from "express";
 import generalSettings from "./config.js";
+import { router } from "./controllers/coffee-controller.js";
 
 const app = express();
-let allCoffesDataArray = [];
 app.use(express.json());
+app.use("/", router);
 
-const readData = () => {
-  const data = fs.readFileSync("store/db.json");
-  return JSON.parse(data.toString());
-};
-const writeData = (newCoffee) => {
-  allCoffesDataArray.push(newCoffee);
-  fs.writeFileSync("store/db.json", JSON.stringify(allCoffesDataArray));
-};
-
-const findCoffeeByCode = (id) => {
-  return allCoffesDataArray.find((coffee) => coffee.id ===id);
-  };
-
-allCoffesDataArray = readData();
-
-app.get(`${generalSettings.baseUrl}/coffee`, (req, res) => {
-  res.send(JSON.stringify(allCoffesDataArray));
-});
-
-app.get(`${generalSettings.baseUrl}/coffee/:code`, (req, res) => {
-  const id = +req.params.id;
-  const requestedCoffee = findCoffeeByCode(id);
-  res.send(JSON.stringify(requestedCoffee));
-});
-
-app.post(`${generalSettings.baseUrl}/coffee`, (req, res) => {
-  writeData(req.body);
-  res.send("works");
-});
-
-app.listen(5000);
-console.log("server is running on port 5000 localhost!");
+app.listen(generalSettings.port);
+console.log(`server is running on port ${generalSettings.port} localhost!`);
